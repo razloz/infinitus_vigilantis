f7688c38596941de4c7c8121c104e9e4072a1ce0
razloz <54073250+razloz@users.noreply.github.com>
2020-03-26 09:29:06 -0500

    Add cartographer cmdline args

--- cartographer.py
+++ cartographer.py
@@ -17,5 +17,19 @@ if __name__ == '__main__':
     print(f'\n{LICENSE}\n{DISCLAIMER}\n')
     print('Loading IVy Cartographer...')
     import source.ivy_cartography as charts
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument("--symbol", help='Symbol for historical data.')
+    p.add_argument("--size", help='Chart size after resampling.')
+    valid_times = ('5Min', '10Min', '15Min', '30Min', '1H', '3H')
+    p.add_argument("--timing", help=f'Valid times are {valid_times}.')
+    args = p.parse_args()
     print('Starting IVy Cartographer...')
-    charts.cartographer()
+    if args.symbol:
+        sym = str(args.symbol)
+        cs = int(args.size) if args.size else 610
+        adj = str(args.timing) if args.timing else '5Min'
+        charts.cartographer(symbol=sym, chart_size=cs, adj_time=adj)
+    else:
+        print('No arguments passed...daemonizing.')
+        charts.cartographer()
