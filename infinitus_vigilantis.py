8599392181172fd5cae194080abf6a79a329b0da
razloz <54073250+razloz@users.noreply.github.com>
2020-03-30 02:00:01 -0500

    GTK update

--- infinitus_vigilantis.py
+++ infinitus_vigilantis.py
@@ -3,19 +3,19 @@
 
 __author__ = 'Daniel Ward'
 __copyright__ = 'Copyright 2020, Daniel Ward'
-__license__ = 'MIT'
-__version__ = '2020.03'
+__license__ = 'GPL v3'
+__version__ = '2020.04'
 __codename__ = 'compass'
 
 
 if __name__ == '__main__':
-    print(f'\n{__doc__}\nVersion: {__version__}')
-    with open('./license/MIT.txt', 'r') as f:
+    print(f'\n{__doc__}\nVersion: {__version__} ({__codename__})')
+    with open('./license/GPLv3.txt', 'r') as f:
         LICENSE = f.read()
     with open('./license/Disclaimer.txt', 'r') as f:
         DISCLAIMER = f.read()
     print(f'\n{LICENSE}\n{DISCLAIMER}\n')
     print('Loading GUI...')
-    import source.ivy_gui_tk as ivy
+    import source.ivy_gui as ivy
     print('Starting IVy GUI...')
-    ivy.start_gui()
+    ivy.start_app()
--- updater.py
+++ updater.py
@@ -3,14 +3,14 @@
 
 __author__ = 'Daniel Ward'
 __copyright__ = 'Copyright 2020, Daniel Ward'
-__license__ = 'MIT'
-__version__ = '2020.03'
+__license__ = 'GPL v3'
+__version__ = '2020.04'
 __codename__ = 'compass'
 
 
 if __name__ == '__main__':
-    print(f'\n{__doc__}\nVersion: {__version__}')
-    with open('./license/MIT.txt', 'r') as f:
+    print(f'\n{__doc__}\nVersion: {__version__} ({__codename__})')
+    with open('./license/GPLv3.txt', 'r') as f:
         LICENSE = f.read()
     with open('./license/Disclaimer.txt', 'r') as f:
         DISCLAIMER = f.read()
