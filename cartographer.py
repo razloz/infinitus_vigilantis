89dc085d3890068c9d3ce1d826aac34c829c0a7a
razloz <54073250+razloz@users.noreply.github.com>
2020-07-23 21:49:36 -0500

    Add --all to cartographer

--- cartographer.py
+++ cartographer.py
@@ -22,33 +22,43 @@ if __name__ == '__main__':
     vt = ('5Min', '10Min', '15Min', '30Min', '1H', '3H')
     p = argparse.ArgumentParser()
     p.add_argument('--symbol', help='Symbol for historical data.')
-    p.add_argument('--size', help='Chart size after resampling.')
-    p.add_argument('--timing', help=f'Valid times are {vt}.')
+    p.add_argument('--size', help='Chart size after resampling. Defaults to 540.')
+    p.add_argument('--timing', help=f'Valid times are {vt}. Defaults to 15Min.')
     p.add_argument('--daemonize', action='store_true',
                    help='Enable daemonized loop.')
+    p.add_argument('--start_date', help='Defaults to 2020-06-01.')
+    p.add_argument('--end_date', help='Defaults to today.')
     p.add_argument('--cheeseless', action='store_true',
                    help='Skip signal generation from the ALL CHEESE.')
-    p.add_argument('--start_date', help='Defaults to 2015-01-01.')
-    p.add_argument('--end_date', help='Defaults to today.')
+    p.add_argument('--all', action='store_true',
+                   help='Generate charts from index.')
     args = p.parse_args()
     today = time.strftime('%Y-%m-%d', time.localtime())
-    s = str(args.start_date) if args.start_date else '2015-01-01'
+    s = str(args.start_date) if args.start_date else '2020-06-01'
     e = str(args.end_date) if args.end_date else today
     chart_args = dict(start_date=s, end_date=e)
-    if args.symbol:
-        chart_args['symbol'] = str(args.symbol)
-    if args.size:
-        chart_args['chart_size'] = int(args.size)
+    chart_args['chart_size'] = int(args.size) if args.size else 540
+    if args.daemonize: chart_args['daemon'] = True
+    if args.cheeseless: chart_args['no_signals'] = True
     if args.timing:
         if args.timing in vt:
             chart_args['adj_time'] = str(args.timing)
-    if args.daemonize:
-        chart_args['daemon'] = True
-    if args.cheeseless:
-        chart_args['no_signals'] = True
-    if len(chart_args) > 0:
-        print(f'Starting argumentative cartographer.')
+        else:
+            chart_args['adj_time'] = '15Min'
+    else:
+        chart_args['adj_time'] = '15Min'
+    if args.symbol:
+        chart_args['symbol'] = str(args.symbol)
+        print('Starting argumentative cartographer.')
         charts.cartographer(**chart_args)
+    elif args.all:
+        import pandas
+        ci_path = './indexes/composite.index'
+        ivy_ndx = pandas.read_csv(ci_path, index_col=0)
+        symbols = ivy_ndx['symbols'].tolist()
+        for symbol in symbols:
+            chart_args['symbol'] = str(symbol)
+            charts.cartographer(**chart_args)
     else:
         print('Starting default cartographer.')
         charts.cartographer()
