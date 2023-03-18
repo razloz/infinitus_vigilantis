Update: fixed gradient nans and stuff and things

source/ivy_candles.py
@@ -512,7 +512,7 @@ class Candelabrum:
             print(prefix, f'{message}.')
         print(prefix, 'The recital comes to an end.')
 
-    def make_offering(self, paterae, cook_time=0, epochs=-1, trim=34):
+    def make_offering(self, paterae, cook_time=None, epochs=-1, trim=34):
         """Spend time with the Norn researching candles."""
         from torch import load
         from torch.nn.utils.rnn import pad_sequence
@@ -540,7 +540,10 @@ class Candelabrum:
         offerings = pad_sequence(
             [t.flip(0) for t in offerings.values()],
             ).transpose(0, 1).flip(2)
-        moirai = ThreeBlindMice(keys, offerings, cook_time=cook_time)
+        if cook_time:
+            moirai = ThreeBlindMice(keys, offerings, cook_time=cook_time)
+        else:
+            moirai = ThreeBlindMice(keys, offerings)
         loop_start = time.time()
         while aeternalis:
             session = moirai.research()

source/ivy_mouse.py
@@ -6,7 +6,7 @@ import traceback
 import matplotlib.pyplot as plt
 import numpy as np
 import source.ivy_commons as icy
-from torch import nn
+from torch import bernoulli, fft, full_like, nn, stack
 from torch.optim import Adagrad
 from torch.utils.data import DataLoader, TensorDataset
 from math import sqrt, inf
@@ -19,13 +19,13 @@ __license__ = 'GPL v3'
 
 class ThreeBlindMice(nn.Module):
     """Let the daughters of necessity shape the candles of the future."""
-    def __init__(self, symbols, offerings, cook_time=0, verbosity=2):
+    def __init__(self, symbols, offerings, cook_time=inf, verbosity=2):
         """Beckon the Norn."""
         super(ThreeBlindMice, self).__init__()
         iota = 1 / 137
         phi = 0.618033988749894
         self._device_type_ = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-        self._device_ = torch.device(self._device_type_)
+        self._device_ = dev = torch.device(self._device_type_)
         self._state_path_ = abspath('./rnn')
         if not exists(self._state_path_): mkdir(self._state_path_)
         self._epochs_path_ = abspath('./rnn/epochs')
@@ -33,53 +33,59 @@ class ThreeBlindMice(nn.Module):
         self._sigil_path_ = abspath('./rnn/sigil')
         if not exists(self._sigil_path_): mkdir(self._sigil_path_)
         self.symbols = list(symbols)
-        self.offerings = offerings.clone().detach()
+        self.offerings = offerings.clone().detach().transpose(0, 1)
         constants = {}
         constants['cook_time'] = cook_time
-        constants['dropout'] = iota
         constants['eps'] = iota * 1e-6
-        constants['n_syms'] = len(self.symbols)
-        constants['input_size'] = constants['n_syms']
-        constants['hidden'] = constants['n_syms']
-        constants['layer'] = 32
-        constants['proj_size'] = constants['n_syms']
-        constants['prob_init'] = 1 / constants['n_syms']
+        constants['hidden'] = 2048
         constants['lr_decay'] = iota / phi
         constants['lr_init'] = iota ** phi
-        constants['mask_prob'] = (phi - 1)
-        constants['momentum'] = phi * phi
-        constants['tolerance'] = (iota * phi) / 3
+        constants['n_features'] = int(self.offerings.shape[2])
+        constants['n_sample'] = 34
+        constants['n_symbols'] = int(self.offerings.shape[1])
+        constants['n_time'] = int(self.offerings.shape[0])
+        constants['n_time'] -= constants['n_sample']
         constants['weight_decay'] = iota / (1 + phi)
         self._constants_ = dict(constants)
+        self.change_stack = (offerings[: , :, -1] * 0.01).clone().detach().H
+        self.change_stack = self.change_stack[constants['n_sample']:].softmax(1)
+        self.offerings = self.offerings[:-constants['n_sample']]
+        self.offerings.requires_grad_(True)
         self._prefix_ = prefix = 'Moirai:'
-        self._symbol_ = None
+        tfloat = torch.float
+        self.mask_p = lambda t: full_like(t, phi, device=dev, dtype=tfloat)
+        self.mask = lambda t: t * bernoulli(self.mask_p(t))
+        self.inscription = fft.rfft
+        self.gate = nn.GLU()
+        self.leaky = nn.LeakyReLU()
+        self.wax_layer = nn.Bilinear(
+            in1_features=constants['n_features'],
+            in2_features=constants['n_features'],
+            out_features=9,
+            bias=True,
+            device=dev,
+            dtype=tfloat,
+            )
         self.cauldron = nn.LSTMCell(
-            input_size=constants['input_size'],
+            input_size=constants['n_symbols'],
             hidden_size=constants['hidden'],
             bias=True,
-            device=self._device_,
-            dtype=torch.float,
+            device=dev,
+            dtype=tfloat,
         )
-        self.wax_layer = torch.nn.Bilinear(
-            in1_features=constants['n_syms'],
-            in2_features=constants['n_syms'],
-            out_features=constants['n_syms'] ** 3,
+        self.wax_coat = nn.LSTMCell(
+            input_size=constants['hidden'],
+            hidden_size=constants['hidden'],
             bias=True,
-            device=self._device_,
-            dtype=torch.float,
-            )
-        self.wax_coat = torch.nn.Conv3d(
-            in_channels=constants['n_syms'] ** 3,
-            out_channels=constants['n_syms'],
-            kernel_size=9,
-            stride=1,
-            padding=0,
-            dilation=1,
-            groups=1,
+            device=dev,
+            dtype=tfloat,
+        )
+        self.wax_seal = nn.Linear(
+            in_features=int(constants['hidden'] / 2),
+            out_features=constants['n_symbols'],
             bias=True,
-            padding_mode='zeros',
-            device=self._device_,
-            dtype=torch.float,
+            device=dev,
+            dtype=tfloat,
             )
         self.loss_fn = nn.HuberLoss(
             reduction='mean',
@@ -94,8 +100,11 @@ class ThreeBlindMice(nn.Module):
             foreach=False,
             maximize=False,
             )
-        self.mask_p = lambda t: torch.full_like(t, phi, device=self._device_)
-        self.mask = lambda t: t * torch.bernoulli(self.mask_p(t))
+        self.zero_loss = torch.zeros(
+            constants['n_symbols'],
+            device=dev,
+            dtype=tfloat,
+            )
         self.epochs = 0
         self.metrics = dict()
         self.verbosity = int(verbosity)
@@ -140,140 +149,77 @@ class ThreeBlindMice(nn.Module):
             if self.verbosity > 2:
                 print(self._prefix_, 'Saved RNN state.')
 
-    def __sealed_candles__(self, candles, study=False):
+    def inscribe_sigil(self, candles, changes, study=True):
         """Let Clotho mold the candles
            Let Lachesis measure the candles
            Let Atropos seal the candles
            Let Awen contain the wax"""
         if study:
             self.train()
-            self.optimizer.zero_grad()
-            return self.forward(candles)
         else:
             self.eval()
-            with torch.no_grad():
-                return self.forward(candles)
-
-    def forward(self, candles):
-        """**bubble*bubble**bubble**"""
-        bubbles = self.mask(candles)
-        self.cauldron(
-        return self.cauldron(
-            self.candelabrum.unsqueeze(0),
-            (self.wax_hidden, self.wax_cell),
-            )[0].log_softmax(1)
-
-    def create_sigil(dataframe, sigil_type='weather'):
-        """Translate dataframe into an arcane sigil."""
-        if sigil_type == 'weather':
-            self.summon_storm(dataframe)
+        mask = self.mask
+        optimizer = self.optimizer
+        optimizer.zero_grad()
+        bubbles = self.wax_layer(candles, candles)
+        bubbles = self.inscription(bubbles)
+        bubbles = stack([bubbles.real, bubbles.imag]).mean(2).mean(0)
+        bubbles = mask(bubbles).softmax(0)
+        bubbles = mask(self.cauldron(bubbles)[0]).softmax(0)
+        bubbles = self.wax_coat(bubbles)[0]
+        bubbles = self.gate(bubbles).tanh()
+        bubbles = self.wax_seal(bubbles).softmax(0)
+        if study:
+            abs_loss = (bubbles - changes).abs()
+            loss = self.loss_fn(abs_loss, self.zero_loss)
+            loss.backward()
+            optimizer.step()
+            return bubbles, loss.item()
+        else:
+            return bubbles
 
     def research(self):
         """Moirai research session, fully stocked with cheese and drinks."""
+        torch.autograd.set_detect_anomaly(True)
         banner = ''.join(['*' for _ in range(80)])
         constants = self._constants_
         cook_time = constants['cook_time']
-        loss_fn = self.loss_fn
+        inscribe_sigil = self.inscribe_sigil
         offerings = self.offerings
-        optimizer = self.optimizer
+        changes = self.change_stack
         prefix = self._prefix_
-        sealed_candles = self.__sealed_candles__
         symbols = self.symbols
-        pt_stack = torch.stack
-        pt_tensor = torch.tensor
         verbosity = self.verbosity
-        symbol_range = range(offerings.shape[0])
-        time_range = range(offerings.shape[1])
+        symbol_range = range(constants['n_symbols'])
+        time_range = range(constants['n_time'])
         cooking = True
         t_cook = time.time()
-        change_stack = offerings[: , :, -1].H * 0.01
         losses = 0
         loss_retry = 0
-        loss_timeout = 50
-        prev_grad = None
+        loss_timeout = 13
         while cooking:
             loss_avg = 0
             for day in time_range:
-                changes = change_stack[day, :]
-                candles = sealed_candles(changes, study=True)
-                changes = changes.log_softmax(0).unsqueeze(0)
-                loss = loss_fn(candles, changes)
-                print('candles', candles)
-                print(candles.shape)
-                print('changes', changes)
-                print(changes.shape)
-                if self.candelabrum.grad is not None:
-                    if self.candelabrum.grad is prev_grad:
-                        print('no grad change!')
-                prev_grad = self.candelabrum.grad
-                loss.backward()
-                optimizer.step()
-                loss_avg += loss.item()
-            loss_avg = loss_avg / (time_range[-1] + 1)
+                candles, loss = inscribe_sigil(offerings[day], changes[day])
+                loss_avg += loss
+            loss_avg = loss_avg / constants['n_time']
             self.epochs += 1
             if loss_avg != losses:
                 loss_retry = 0
                 losses = loss_avg
-                print(banner)
-                for i in symbol_range:
-                    print(prefix, symbols[i], candles[i])
-                print(prefix, 'loss', loss_avg)
-                print(banner)
+                print(prefix, 'loss_avg', loss_avg)
             else:
                 loss_retry += 1
                 if loss_retry == loss_timeout:
+                    candles = inscribe_sigil(offerings, None, study=False)
+                    print(banner)
+                    for i in symbol_range:
+                        print(prefix, symbols[i], candles[i])
                     print(prefix, 'losses', losses)
+                    print(banner)
                     cooking = False
             elapsed = time.time() - t_cook
             if elapsed >= cook_time:
                 cooking = False
         self.__manage_state__(call_type=1)
         return True
-
-    def summon_storm(sigil):
-        """The clouds gather to the sounds of song."""
-        pass
-
-
-
-
-
-import torch
-dev_type = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-dev = torch.device(dev_type)
-phi = 0.618033988749894
-tfloat = torch.float
-mask_p = lambda t: torch.full_like(t, phi, device=dev, dtype=tfloat)
-mask = lambda t: t * torch.bernoulli(mask_p(t))
-fft = torch.fft
-inscription = fft.rfft
-wax_layer = torch.nn.Bilinear(
-    in1_features=36,
-    in2_features=36,
-    out_features=9,
-    bias=True,
-    device=dev,
-    dtype=tfloat,
-    )
-cauldron = torch.nn.LSTMCell(
-    input_size=171,
-    hidden_size=2048,
-    bias=True,
-    device=dev,
-    dtype=tfloat,
-)
-wax_coat = torch.nn.LSTMCell(
-    input_size=2048,
-    hidden_size=2048,
-    bias=True,
-    device=dev,
-    dtype=tfloat,
-)
-x = torch.randn(171, 36, device=dev, dtype=tfloat).log_softmax(1)
-wax = wax_layer(x, x).log_softmax(1)
-sigil = inscription(wax)
-sigil = torch.stack([sigil.real, sigil.imag])
-sigil = ((1 / sigil).mean(2) ** -1).mean(0).sin()
-sigil = mask(sigil).log_softmax(0)
-candles = cauldron(sigil)[0]
-candles = wax_coat(mask(candles))[0]