539d54308a3236b5ad4abc17366ce584b7d7d39c
razloz <54073250+razloz@users.noreply.github.com>
2023-03-31 21:20:18 -0500

    removed masking

--- updater.py
+++ updater.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     elif args.pick:
         candelabrum.pick_candles(25)
     elif args.study:
-        candelabrum.make_offering(None, cook_time=900, epochs=-1)
+        candelabrum.make_offering(None, cook_time=3600, epochs=-1)
