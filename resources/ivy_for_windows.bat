3403b9d879eacae4069044cebbf0f58df17e6c23
razloz <54073250+razloz@users.noreply.github.com>
2023-11-08 20:02:56 -0600

    Delete resources directory

--- resources/ivy_for_windows.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Program Files\infinitus_vigilantis-master\
-C:\Program Files\infinitus_vigilantis-master\.env\Scripts\python.exe "C:\Program Files\infinitus_vigilantis-master\infinitus_vigilantis.py --start_learning"
